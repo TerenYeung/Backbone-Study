9affc1747d3b39009c7a0f551a34df10cca0e9cf
TerenYeung <yangjialong@ifanr.com>
2017-04-13 21:32:19 +0800

    init my backbone-todos

--- mywork/todos.js
+++ mywork/todos.js
@@ -1,94 +1,75 @@
 $(function(){
 
-    //Backbone -> MVC框架
-
-    //如何将一个todosMVC使用MVC框架搭建出来？
-
-    //Model类：定义每一个todoItem的状态，相当于数据表中的一条记录
-        /*todoItem类定义如下：
-
-            let Todo = Backbone.Model.extend({});
-
-            为app的todoItem定义一个类
-
-            Todo类包含的状态有：
-
-                Backbone.Model.extend({
-
-                    idAttribute: '_id', //定义每一个事项唯一标识
-
-                    defaults: {
-                        title: '',  //待办事项
-                        done: false, //完成情况
-                        order: 0, //事项出现的先后顺序
-                    },
-
-                    initialize: function(){
-                        id: _.uniqueId('todo_'), //为每个todo创建唯一id,
-
-                    }
-
-                })
-
-
-        */
-
-	/*
-	 *
-	 *
-	 *
-	 */
-
     let Todo = Backbone.Model.extend({
 
-        idAttribute: '_id',
-
-        default: function(){
-
-            return {
-                title: '',
-                done: false,
-                order: 0,
-            }
+        defaults: {
+            title: 'Hello World',
+            done: false,
+            order: 0,
         },
 
-        initialzie: function(){
+        initialize: function(){
 
-            let todoId = _.uniqueId('todo_');
-            this.set('_id',todoId);
+            // this.
         }
-    });
 
-    //Collection类: 定义模型的有序集合，相当于数据库中的表
-        /*
-        * 由所有todo项目组成的集合只有两种状态：
-        * 完成和未完成
-        * done和undone
-        * */
+    })
 
     let Todos = Backbone.Collection.extend({
 
-        model: Todo, //定义集合中包含的模型类
+        model: Todo,
 
-        done: function(){
+    });
 
-            let doneArr = _.filter(function(todo){
-                return todo.get('done');
-            })
+    let TodoView = Backbone.View.extend({
 
-            return doneArr;
-        },
+        tagName: 'li',
 
-        undone: function(){
-            let undoneArr = _.without.apply(this,this.done());
+        className: 'todo-item',
 
-            return undoneArr;
-        }
+
+
     })
 
-    //TodoView类: 定义每个todo项目的视图
-        /*
-        *
-        *
-        * */
+    let AppView = Backbone.View.extend({
+
+        //指定AppView类挂载的DOM节点
+        el: '#todo-app',
+
+        initialize: function(){
+
+            this.todos = new Todos,
+            this.$input = this.$el.find('#new-todo');
+
+        },
+
+        events: {
+            'keypress #new-todo' : 'addItem'
+        },
+
+        addItem: function(e){
+
+            if(e.keyCode != 13) return;
+
+            let title = this.$input.val();
+            if(!title) return;
+
+            let todo = new Todo({title: title});
+
+            this.todos.add(todo)
+
+            console.log(this.todos.models)
+
+        }
+
+
+        // render: function(){
+        //     console.log("I'm rendering...");
+        // },
+
+    })
+
+
+    let app = new AppView;
+
 })
